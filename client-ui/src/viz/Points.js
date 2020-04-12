77afdf127e142147a1ce34521900a7d5f5d24b8b
Martin Blech <78768+martinblech@users.noreply.github.com>
2020-04-11 18:27:31 -0700

    Normalize audio power.

--- client-ui/src/viz/Points.js
+++ client-ui/src/viz/Points.js
@@ -67,7 +67,7 @@ export class Points extends Visualizer {
         positions[3 * k + 1] = y;
         positions[3 * k + 2] = z;
 
-        var intensity = (0.1 + 0.2 * y + 2.0 * this.intensity) * 3;
+        var intensity = (.1 + .2 * y + .3 * this.intensity) * 3;
         colors[3 * k] = baseColor.r * intensity;
         colors[3 * k + 1] = baseColor.g * intensity;
         colors[3 * k + 2] = baseColor.b * intensity;
@@ -95,19 +95,34 @@ export class Points extends Visualizer {
   onAudioData(samples, fft) {
     super.onAudioData(samples, fft);
     let max = 0;
+    let sum = 0;
     for (let i = 0; i < samples.length; i++) {
       const sample = (samples[i]/128) - 1;
       const absSample = Math.abs(sample);
       if (absSample > max) {
         max = absSample;
       }
+      sum += absSample;
     }
+    const avg = sum / samples.length;
     if (!this.max || max > this.max) {
       this.max = max;
     } else {
       this.max = 0.7 * this.max + 0.3 * max;
     }
-    this.intensity = Math.pow(this.max, 2);
+    if (!this.slowMax || max > this.slowMax) {
+      this.slowMax = max;
+    } else {
+      this.slowMax = 0.9 * this.slowMax + 0.1 * max;
+    }
+    if (!this.avg) {
+      this.avg = avg;
+    } else {
+      this.avg = 0.7 * this.avg + 0.3 * avg;
+    }
+    let norm = this.slowMax - this.avg;
+    let normalizedPower = norm? (this.max - this.avg) / norm : 0;
+    this.intensity = Math.pow(normalizedPower, 2);
     this.updatePositionsAndColors();
   }
 }
