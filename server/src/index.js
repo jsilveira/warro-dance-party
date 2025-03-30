0913b0a711cde26e1b3ceabfc1fdfd00150f03fc
jsilveira <jsilveira@gmail.com>
2025-03-30 12:18:06 -0300

    Update package lock

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
-const port = app.get('port');
+
+const port = process.env.PORT || app.get('port');
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-app.listen(port).then(() =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+console.log("Listenning in port "+port);
+
+const server = app.listen(port).then(() => {
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port);
+});
+
+// Handle Heroku's SIGTERM signal
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received. Closing HTTP server...');
+  server.close(() => {
+    logger.info('HTTP server closed');
+    process.exit(0);
+  });
+});
