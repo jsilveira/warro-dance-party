dddef188cce06619c7f9703a7834a23331cb06cc
jsilveira <jsilveira@gmail.com>
2026-05-01 23:37:25 -0300

    Update favicon and metadata for rebranding; add /agenda env link

--- server/src/app.js
+++ server/src/app.js
@@ -94,11 +94,11 @@ app.get('/avatars/:id', (req, res) => {
 });
 
 app.get('/agenda', (req, res) => {
-  res.redirect(307, 'https://calendar.google.com/calendar/embed?src=pampawarro%40gmail.com&ctz=America%2FArgentina%2FBuenos_Aires');
+  res.redirect(307, process.env.AGENDA_URL || 'https://calendar.google.com/calendar/u/0?cid=YzAxOGM0NDM0OWQ5ZTVkYTU3OGRjZTc0NmI2OWRiMzkyNWZkNmRjZGJmZDk4NjExMjI0NTk0ZDk2NzdhZjg0YUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t');
 });
 
 app.get('/agenda-admin', (req, res) => {
-  res.redirect(307, 'https://teamup.com/ksekns8wj4coffwxxn');
+  res.redirect(307, process.env.AGENDA_ADMIN_URL || 'https://teamup.com/ksekns8wj4coffwxxn');
 });
 
 app.get('/', (req, res) => {
